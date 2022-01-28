313a75c5094f4a41e35c44ebadbcb5f22534e1a4
Subbotin Valery <subbotin.v@uspect.ru>
2022-01-28 14:51:53 +0300

    test

--- src/js/components/_ne-cirk-section.js
+++ src/js/components/_ne-cirk-section.js
@@ -2,59 +2,56 @@ import anime from "../vendor/anime.min";
 
 const girlWrapper = document.querySelector(".girl__wrapper");
 const girlInner = document.querySelector(".girl__inner");
-const girl = document.querySelector(".girl");
+const girl = document.querySelector(".girl__wrapper .girl");
+
+const handWrapper = document.querySelector(".hand__wrapper");
 
 const neCirkTitleRegular = document.querySelectorAll(
-  ".ne-cirk__title:not(.outline)"
+  ".ne-cirk__title *:not(* > .outline)"
 );
 const neCirkTitleOutline = document.querySelector(
-  ".ne-cirk__title .italic .outline"
+  ".ne-cirk__title * > .outline"
 );
 
-const neCirkTimeline = anime
-  .timeline({ autoplay: false })
+const neCirkTitleTimeline = anime
+  .timeline({ autoplay: false, easing: "easeInOutQuad" })
   .add(
     {
-      targets: girlWrapper,
-      top: ["60%", "50%"],
-      right: ["30%", "50%"],
-      duration: 400,
-      easing: "linear",
+      targets: neCirkTitleRegular,
+      translateX: "-100%",
+      duration: 600,
     },
-    0
-  )
-  .add(
-    {
-      targets: girl,
-      scale: [1.5, 0.25],
-      duration: 400,
-      easing: "linear",
-    },
-    0
+    100
   )
   .add(
     {
       targets: neCirkTitleOutline,
-      translateX: 700,
-      duration: 400,
-      easing: "linear",
+      translateX: "900%",
+      duration: 600,
     },
-    "-=300"
+    100
   );
 
+const neCirkTimeline = anime.timeline({ autoplay: false }).add({
+  targets: girlWrapper,
+  scale: [1.5, 0.6],
+  top: ["-60%", "50%"],
+  right: ["-30%", "-50%"],
+  duration: 2000,
+});
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log(neCirkTitleRegular);
+  console.log(neCirkTitleOutline);
   const neCirkGirlRotate = anime({
     targets: girlInner,
     rotate: [-30, 30],
-    translateX: ["-5%", "5%"],
-    translateY: ["10", "-10"],
     duration: 2000,
     loop: true,
     direction: "alternate",
     easing: "easeInOutQuad",
   });
   window.addEventListener("scroll", () => {
+    neCirkTitleTimeline.seek(window.pageYOffset);
     neCirkTimeline.seek(window.pageYOffset);
   });
 });
