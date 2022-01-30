d4a0ecbaa30568a25d1c277b725d1222821aef0a
Subbotin Valery <subbotin.v@uspect.ru>
2022-01-31 00:52:28 +0300

    404 page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 import "./_vendor";
+
+//common
+import "./components/_burger";
+import "./components/_star";
+import "./components/_clouds";
+import "./components/_juggler";
+
+//
 import "./components/_ne-cirk-section";
-import "./components/_burger.js";
