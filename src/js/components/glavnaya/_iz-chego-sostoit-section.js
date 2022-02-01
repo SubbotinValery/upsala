4c59b0a0bc46dff2339f795893b8cb3a1b292750
Subbotin Valery <subbotin.v@uspect.ru>
2022-02-01 14:09:52 +0300

    glavnaya fix iz-chego-sostoit section

--- src/js/components/glavnaya/_iz-chego-sostoit-section.js
+++ src/js/components/glavnaya/_iz-chego-sostoit-section.js
@@ -5,9 +5,11 @@ for (let i = 0; i < list.length; i++) {
   list[i].addEventListener("mouseover", function () {
     preview[i].style.transform = "translate(-50%, -50%) rotate(360deg)";
     preview[i].style.width = "100%";
+    preview[i].style.opacity = 1;
   });
   list[i].addEventListener("mouseout", function () {
     preview[i].style.transform = "translate(-50%, -50%) rotate(0deg)";
     preview[i].style.width = "0%";
+    preview[i].style.opacity = 0;
   });
 }
