3ae4a711638e7ab700b8fb6521bf7128e6f97846
Subbotin Valery <subbotin.v@uspect.ru>
2022-02-02 05:10:25 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,12 @@ import "./components/_accordion";
 import "./components/_clouds";
 import "./components/tree";
 
-//index.html
-// import "./components/glavnaya/_ne-cirk-section";
-// import "./components/glavnaya/_video-section";
-// import "./components/glavnaya/_about-section";
-// import "./components/glavnaya/_iz-chego-sostoit-section";
-// import "./components/glavnaya/_huliganskii-proekt-section";
+// index.html;
+import "./components/glavnaya/_ne-cirk-section";
+import "./components/glavnaya/_video-section";
+import "./components/glavnaya/_about-section";
+import "./components/glavnaya/_iz-chego-sostoit-section";
+import "./components/glavnaya/_huliganskii-proekt-section";
 
 //komnda.html
 import "./components/_komanda-slider";
