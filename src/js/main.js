f2efa8592ebc0eed501944644acbc66d5a24cdfb
Subbotin Valery <subbotin.v@uspect.ru>
2022-03-10 16:08:31 +0300

    ашч

--- src/js/main.js
+++ src/js/main.js
@@ -7,32 +7,32 @@ import "./components/_tab";
 import "./components/_expand-vertical";
 
 // index.html;
-// import "./components/glavnaya/_glavnaya";
-// import "./components/glavnaya/_ne-cirk-section";
-// import "./components/glavnaya/_video-section";
-// import "./components/glavnaya/_about-section";
-// import "./components/glavnaya/_iz-chego-sostoit-section";
-// import "./components/glavnaya/_huliganskii-proekt-section";
-// import "./components/glavnaya/_proftruppa-section";
-// import "./components/glavnaya/_incluzivnii-proekt-section";
-// import "./components/glavnaya/_chto-v-rezultate-section";
-// import "./components/glavnaya/_kulturnii-producti-section";
-// import "./components/glavnaya/_smi-o-nas-section";
-// import "./components/glavnaya/_chto-proishodit-section";
+import "./components/glavnaya/_glavnaya";
+import "./components/glavnaya/_ne-cirk-section";
+import "./components/glavnaya/_video-section";
+import "./components/glavnaya/_about-section";
+import "./components/glavnaya/_iz-chego-sostoit-section";
+import "./components/glavnaya/_huliganskii-proekt-section";
+import "./components/glavnaya/_proftruppa-section";
+import "./components/glavnaya/_incluzivnii-proekt-section";
+import "./components/glavnaya/_chto-v-rezultate-section";
+import "./components/glavnaya/_kulturnii-producti-section";
+import "./components/glavnaya/_smi-o-nas-section";
+import "./components/glavnaya/_chto-proishodit-section";
 
-// import "./components/glavnaya/_chto-v-rezultate-slider";
-// import "./components/glavnaya/_kulturnii-producti-slider";
-// import "./components/glavnaya/_chto-proishodit-slider";
-// import "./components/glavnaya/_nasha-istoria-slider";
+import "./components/glavnaya/_chto-v-rezultate-slider";
+import "./components/glavnaya/_kulturnii-producti-slider";
+import "./components/glavnaya/_chto-proishodit-slider";
+import "./components/glavnaya/_nasha-istoria-slider";
 
 //komanda.html
-import "./components/_komanda-slider";
+// import "./components/_komanda-slider";
 
-//not-found.html
-import "./components/not-found/_not-found";
+// //not-found.html
+// import "./components/not-found/_not-found";
 
-//afisha
-import "./components/afisha/_afisha";
+// //afisha
+// import "./components/afisha/_afisha";
 
-//support
-import "./components/support/support";
+// //support
+// import "./components/support/support";
