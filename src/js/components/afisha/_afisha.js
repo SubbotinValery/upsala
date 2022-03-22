15407869df16723a1e2343e4888a5fc3c56acbdc
Subbotin Valery <subbotin.v@uspect.ru>
2022-03-22 07:50:41 +0300

    fix

--- src/js/components/afisha/_afisha.js
+++ src/js/components/afisha/_afisha.js
@@ -1,70 +1,68 @@
 let actorSplide;
 let actorPrev, actorNext;
 
-document.addEventListener("DOMContentLoaded", function () {
-  actorPrev = document.querySelector("#afishaActorSlider .previous-button");
-  actorNext = document.querySelector("#afishaActorSlider .next-button");
+const afishaActorSlider = document.querySelector("#afishaActorSlider");
 
-  actorSplide = new Splide("#afishaActorSlider .splide", {
-    arrows: false,
-    perPage: 2,
-    type: "loop",
-    pagination: true,
-    keyboard: false,
-    slideFocus: false,
-    breakpoints: {
-      1024: {
-        perPage: 1,
+if (afishaActorSlider !== null) {
+  document.addEventListener("DOMContentLoaded", function () {
+    actorPrev = document.querySelector("#afishaActorSlider .previous-button");
+    actorNext = document.querySelector("#afishaActorSlider .next-button");
+
+    actorSplide = new Splide("#afishaActorSlider .splide", {
+      arrows: false,
+      perPage: 2,
+      type: "loop",
+      pagination: true,
+      keyboard: false,
+      slideFocus: false,
+      breakpoints: {
+        1024: {
+          perPage: 1,
+        },
       },
-    },
-  }).mount();
+    }).mount();
 
-  actorSplide.on("move", function () {
-    var slides = document.querySelectorAll(
-      "#afishaActorSlider .splide .splide__slide"
-    );
+    actorSplide.on("move", function () {
+      var slides = document.querySelectorAll(
+        "#afishaActorSlider .splide .splide__slide"
+      );
 
-    slides.forEach(function (slide) {
-      slide.classList.add("is-visible");
+      slides.forEach(function (slide) {
+        slide.classList.add("is-visible");
+      });
     });
-  });
 
-  actorPrev.addEventListener("click", function (e) {
-    actorSplide.go("<");
-  });
+    actorPrev.addEventListener("click", function (e) {
+      actorSplide.go("<");
+    });
 
-  actorNext.addEventListener("click", function (e) {
-    actorSplide.go(">");
-  });
+    actorNext.addEventListener("click", function (e) {
+      actorSplide.go(">");
+    });
 
-  const visibles = document.querySelectorAll(
-    "#afishaActorSlider .splide .splide__slide.is-visible"
-  );
-});
+    const visibles = document.querySelectorAll(
+      "#afishaActorSlider .splide .splide__slide.is-visible"
+    );
+  });
+}
 
 let photoSplide;
 
-document.addEventListener("DOMContentLoaded", function () {
-  splide = new Splide("#afishaPhotoSlider .splide", {
-    arrows: false,
-    perPage: 2,
-    pagination: true,
-    keyboard: false,
-    slideFocus: false,
-    breakpoints: {
-      1024: {
-        perPage: 1,
+const afishaPhotoSlider = document.querySelector("#afishaPhotoSlider ");
+
+if (afishaPhotoSlider !== null) {
+  document.addEventListener("DOMContentLoaded", function () {
+    splide = new Splide("#afishaPhotoSlider .splide", {
+      arrows: false,
+      perPage: 2,
+      pagination: true,
+      keyboard: false,
+      slideFocus: false,
+      breakpoints: {
+        1024: {
+          perPage: 1,
+        },
       },
-    },
-  }).mount();
-
-  // photoSplide.on("move", function () {
-  //   var slides = document.querySelectorAll(
-  //     "#afishaPhotoSlider .splide .splide__slide"
-  //   );
-
-  //   slides.forEach(function (slide) {
-  //     slide.classList.add("is-visible");
-  //   });
-  // });
-});
+    }).mount();
+  });
+}
