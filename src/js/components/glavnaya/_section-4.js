d6ca3070a85fb5f8f0339056ea3a7a5fab6e41d6
Subbotin Valery <subbotin.v@uspect.ru>
2022-04-06 23:27:44 +0300

    fix

--- src/js/components/glavnaya/_section-4.js
+++ src/js/components/glavnaya/_section-4.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const url = document.URL;
+  const anchor = url.split("#")[1];
+  const anchoredBlock = document.getElementById(anchor);
+
   const section = document.querySelector(".section-4");
 
   const preview = section.querySelectorAll(".preview__block .preview");
-  const list = section.querySelectorAll(".nav__list .nav__item");
+  const list = section.querySelectorAll(".nav__list .nav__link");
 
   for (let i = 0; i < list.length; i++) {
     list[i].addEventListener("mouseover", function () {
@@ -16,62 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
       preview[i].style.opacity = 0;
     });
   }
-  const section5 = document.querySelector(".section-5").offsetTop;
-  const section6 = document.querySelector(".section-6").offsetTop;
-  const section7 = document.querySelector(".section-7").offsetTop;
-  const section8 = document.querySelector(".section-8").offsetTop;
-  const section9 = document.querySelector(".section-9").offsetTop;
-  const section10 = document.querySelector(".section-10").offsetTop;
-  const section11 = document.querySelector(".section-11").offsetTop;
-  const section12 = document.querySelector(".section-12").offsetTop;
 
-  list.forEach((element) => {
-    const title = element.textContent.toLowerCase().split(" ").join("");
-    console.log(title);
-    switch (title) {
-      case "хулиганскийпроект":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section5, behavior: "smooth" });
-        });
-        break;
-      case "профтруппа":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section6, behavior: "smooth" });
-        });
-        break;
-      case "инклюзивныйпроект":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section7, behavior: "smooth" });
-        });
-        break;
-      case "чтов результате?":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section8, behavior: "smooth" });
-        });
-        break;
-      case "культурныепродукты":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section9, behavior: "smooth" });
-        });
-        break;
-      case "смио нас":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section10, behavior: "smooth" });
-        });
-        break;
-      case "а чтопроисходитсейчас":
-        element.addEventListener("click", function () {
-          console.log(section11);
-          window.scroll({ top: section11, behavior: "smooth" });
-        });
-        break;
-      case "нашаистория":
-        element.addEventListener("click", function () {
-          window.scroll({ top: section12, behavior: "smooth" });
-        });
-        break;
-      default:
-        break;
-    }
-  });
+  if (anchoredBlock != null) {
+    window.scrollTo({
+      top: anchoredBlock.offsetTop - 150,
+      behavior: "smooth",
+    });
+  }
 });
